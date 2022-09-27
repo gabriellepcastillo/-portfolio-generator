4d24de8a01af8f6c9fd203aed556112ce86f26f1
gabriellepcastillo@gmail.com <Gabriellepcastillo@gmail.com>
2022-09-27 13:01:52 -0400

    added gitgnore and node-module and package.json

---
+++ app.js
@@ -0,0 +1,22 @@
+const moment = require('moment');
+const dateTime = moment();
+console.log(dateTime.format("MMM do YY"));
+
+const fs = require('fs');
+const generatePage = require('./src/page-template');
+
+const profileDataArgs = process.argv.slice(2);
+
+console.log(profileDataArgs);
+
+const [name, github] = profileDataArgs;
+
+console.log(name, github);
+
+const pageHTML = generatePage(name, github);
+
+fs.writeFile('./index.html', pageHTML, err => {
+  if (err) throw err;
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
